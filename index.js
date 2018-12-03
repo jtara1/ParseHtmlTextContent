36de63b34621ed68e5029303b1eee0019776d2a7
James T <jamest@earny.co>
2018-12-03 15:40:41 -0800

    fix: return empty str if empty str given

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities();
 
 module.exports = function parseTextContent(htmlText) {
+  if (!htmlText) return '';
+  
   // parse out html tags that contain textContent that is embedded src code or css
   htmlText = htmlText.replace(/<head[\s\S]*>[\s\S]*<\/head>/ig, '');
   htmlText = htmlText.replace(/<(?:no)?script>[\s\S]*?<\/(?:no)?script>/g, '');
