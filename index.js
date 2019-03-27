8278d5bc84fcb1db21156f132721ac3f637e15de
James T <jamest@earny.co>
2019-03-27 13:57:58 -0700

    add: parse out style tag & its text content

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities();
 
-module.exports = function parseTextContent(htmlText) {
+/**
+ *
+ * @param htmlText
+ * @param opts
+ * @param opts.lineFilterFuncs
+ * @param opts.flatten
+ * @returns {*}
+ */
+module.exports = function parseTextContent(
+  htmlText,
+  opts = { lineFilterFuncs: [], flatten: false })
+{
   if (!htmlText) return '';
-  
+
   // parse out html tags that contain textContent that is embedded src code or css
-  htmlText = htmlText.replace(/<head[\s\S]*>[\s\S]*<\/head>/ig, '');
+  htmlText = htmlText.replace(/<head[\s\S]*?>[\s\S]*?<\/head>/ig, '');
   htmlText = htmlText.replace(/<(?:no)?script>[\s\S]*?<\/(?:no)?script>/g, '');
+  htmlText = htmlText.replace(/<style.*?>[\s\S]*?<\/style>/g, '');
   htmlText = htmlText.replace(/<[\s\S]*?>/g, ' ');
 
   // parse out bulky whitespace chars
